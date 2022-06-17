cceaecdece08aeac5f84932d1dc4e3b88ddc3087
sRivera23 <106624602+sRivera23@users.noreply.github.com>
2022-06-17 12:15:03 -0500

    Add files via upload

---
+++ Reto3/src/main/java/concesionario/Reto3/Llanta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package concesionario.Reto3;
+
+/**
+ *
+ * @author Santiago Rivera y Santiago Alexander Losada
+ */
+public class Llanta {
+    
+    private String marca;
+    private String referencia;
+    private int peso;
+    
+}
---
+++ Reto3/src/main/java/concesionario/Reto3/Motor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package concesionario.Reto3;
+
+/**
+ *
+ * @author Santiago Rivera y Santiago Alexander Losada
+ */
+public class Motor {
+    
+    private int cilindraje;
+    private String marca;
+    private String referencia;
+    private int peso;
+    private String descripcionMotor;
+    
+}
